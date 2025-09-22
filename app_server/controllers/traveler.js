0c706c64bf2f744cf53a28749d1274e19a56f404
Dea Tahirllari <dea.tahirllari@yahoo.com>
2025-09-21 20:11:59 -0400

    Module 3: /travel renders trips from JSON via HBS (MVC)

--- app_server/controllers/traveler.js
+++ app_server/controllers/traveler.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+
 exports.travel = (req, res) => {
-  res.render('travel', { title: 'Travel (HBS)' });
+  const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+  res.render('travel', { title: 'Travel', trips });
 };
